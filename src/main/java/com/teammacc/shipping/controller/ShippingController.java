49fa9d4edccfa9fe138a6ef077d2cb2247fa625d
Ingrid Guedes Teles Coutinho <ingrid.teles@serpro.gov.br>
2020-12-22 13:01:12 -0300

    ajuste url e api docs

--- src/main/java/com/teammacc/shipping/controller/ShippingController.java
+++ src/main/java/com/teammacc/shipping/controller/ShippingController.java
@@ -3,13 +3,11 @@ package com.teammacc.shipping.controller;
 import com.teammacc.shipping.data.vo.ShippingVO;
 import com.teammacc.shipping.service.ShippingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/shipping")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/")
 public class ShippingController {
 
     private final ShippingService shippingService;
@@ -19,7 +17,7 @@ public class ShippingController {
         this.shippingService = shippingService;
     }
 
-    @GetMapping(value = "/{cep}")
+    @GetMapping(value = "/consulta/{cep}")
     public ShippingVO findShipping(@PathVariable("cep") String cep) {
         return shippingService.findShipping(cep);
     }
--- src/main/java/com/teammacc/shipping/data/vo/ShippingVO.java
+++ src/main/java/com/teammacc/shipping/data/vo/ShippingVO.java
@@ -1,24 +1,16 @@
 package com.teammacc.shipping.data.vo;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
-import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 
-@JsonPropertyOrder({"shippingCost", "deliveryEstimated"})
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class ShippingVO extends RepresentationModel<ShippingVO> implements Serializable {
-
-    @JsonProperty("shippingCost")
+public class ShippingVO implements Serializable {
     private Double shippingCost;
-
-    @JsonProperty("deliveryEstimated")
     private Integer deliveryEstimated;
 }
